db99489405e98c315f7e70e305fc26e1c2bc7ee6
Roy Lopez <ryhsu2@gmail.com>
2026-08-18 19:26:14 +0200

    feat: Implement tab navigation layout and screens for meal tracking app
    - Added TabLayout component for managing tab navigation.
    - Created AddMealsScreen for adding new meals.
    - Developed HomeScreen with a header and link to meals.
    - Implemented MealsScreen to display all meals with a link to add a new meal.
    - Removed unused Explore screen and related components.
    - Updated RootLayout to use Stack navigation instead of ThemeProvider.
    - Removed deprecated components and styles related to the previous navigation setup.
    - Introduced global styles and color constants for consistent theming.

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,18 +1,20 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { useColorScheme } from 'react-native';
+import { colors } from "@/styles/global";
+import { Stack } from "expo-router";
 
-import { AnimatedSplashOverlay } from '@/components/animated-icon';
-import AppTabs from '@/components/app-tabs';
-
-SplashScreen.preventAutoHideAsync();
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function RootLayout() {
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <AnimatedSplashOverlay />
-      <AppTabs />
-    </ThemeProvider>
+    <Stack
+      screenOptions={{
+        headerStyle: { backgroundColor: colors.header },
+        headerTintColor: "#fff",
+      }}
+    >
+      <Stack.Screen
+        name="index"
+        options={{ headerShown: false, title: "Home" }}
+      />
+      <Stack.Screen options={{ title: "Meals" }} name="meals" />
+      <Stack.Screen options={{ title: "Add Meal" }} name="add-meal" />
+    </Stack>
   );
 }
